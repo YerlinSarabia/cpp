350b937f53344bb5347876581522c071c97a9fa4
Yerlin Sarabia <yerlin.sarabia@unah.hn>
2020-07-11 21:41:42 -0500

    agregando variables.cpp

--- Ejercicios/Ejercicio3 - Variables/variables.cpp
+++ Ejercicios/Ejercicio3 - Variables/variables.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int numeroEntero;
+    numeroEntero = 15;
+
+    cout << numeroEntero;
+
+    numeroEntero = 18;
+
+    cout << numeroEntero;
     return 0;
 }
