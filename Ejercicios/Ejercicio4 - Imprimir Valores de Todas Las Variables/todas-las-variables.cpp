b9e5aea947868637ffa8aa01f3be4e9a27543fde
Yerlin Sarabia <yerlin.sarabia@unah.hn>
2020-07-13 19:46:28 -0500

    agregando todas-las-variables.cpp

---
+++ Ejercicios/Ejercicio4 - Imprimir Valores de Todas Las Variables/todas-las-variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    bool valorBoolean;
+    valorBoolean = true;
+
+    int valorEntero = 15;    
+    double valorDouble = 20.99;
+    string valorString = "Hola como estan";
+    char valorChar = B; 
+
+    return 0;
+}
