2f25e73f437cf02e1fac2c3ec291fa34c8268ece
Yerlin Sarabia <yerlin.sarabia@unah.hn>
2020-08-11 00:08:19 -0500

    agregando raiz-cuadrada.cpp

---
+++ Ejercicios/Ejercicio50 - Tarea8/raiz-cuadrada.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <math.h>       
+
+
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+
+{
+
+    double numero = 0, raizCuadrada = 0;
+
+
+
+    cout << "Ingrese un numero: "<<endl;
+
+    cin >> numero;
+
+
+    raizCuadrada = sqrt(numero);
+
+
+    cout << "La raiz cuadrada es: " << raizCuadrada;
+
+     
+
+    return 0;
+
+}
