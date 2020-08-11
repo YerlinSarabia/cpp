b54622f85e8c5bd9169db9f5eff5cb8872a5e601
Yerlin Sarabia <yerlin.sarabia@unah.hn>
2020-08-10 23:31:48 -0500

    agregando factura.cpp agregando factura.h agregando main.cpp agregando menu.cpp agregando productos.cpp

---
+++ Ejercicios/Ejercicio45 - Tarea7/factura.h
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+
+
+void agregarProducto(string descripcion, int cantidad, double precio);
+
+void imprimirFactura();
+
+void productos(int opcion);
+
+void menu();
---
+++ Ejercicios/Ejercicio45 - Tarea7/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include "factura.h"
+
+
+
+using namespace std;
+
+
+
+int main(int argc, char const *argv[])
+
+{
+
+    menu();
+
+ 
+
+    return 0;
+
+}
