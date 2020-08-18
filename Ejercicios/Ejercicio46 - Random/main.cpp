987e3e82e522ce433fc37c3650936dd43d5da9f5
Yerlin Sarabia <yerlin.sarabia@unah.hn>
2020-08-18 00:11:22 -0500

    agregando main.cpp

---
+++ Ejercicios/Ejercicio46 - Random/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+
+{
+
+    int numero = 0;
+
+
+//inicializa el numero random
+
+    srand(time(NULL));
+
+
+   for (int i = 0; i < 20; i++)
+
+   {
+
+        // genera un numero entre uno y 10
+
+        numero = rand() % 100 + 1;
+
+        cout << "Numero al azar: " <<  numero << endl;   
+
+   }
+    
+
+    return 0;
+}
