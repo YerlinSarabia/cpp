2c59b3faae6ed07170d0c791e9998256d5f2c86f
Yerlin Sarabia <yerlin.sarabia@unah.hn>
2020-08-17 23:40:44 -0500

    agregando factura.cpp

---
+++ Ejercicios/Ejercicio42 - Punto de Venta Parte III/factura.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+double subtotal;
+
+string listaProductos;
+
+
+void agregarProducto(string descripcion, int cantidad, double precio)
+
+{
+
+    listaProductos = listaProductos + descripcion + '\n';
+
+    subtotal = subtotal + (cantidad * precio);
+
+}
